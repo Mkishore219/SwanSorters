3d968ed6399a076d3dd5abd92d1a60627d34fed2
Mkishore219 <152087766+Mkishore219@users.noreply.github.com>
2025-04-14 21:12:34 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+
+function loaderAnimation() {
+    
+    const tl1 = gsap.timeline();
+    tl1.to("#loader .loader-div1", {
+        y: "-120%",
+        duration: 1.5,
+        ease: "power1.out",
+        delay:0.5
+    },"loader");
+    tl1.to("#loader .loader-div2", {
+        y: "120%",
+        duration: 1.5,
+        ease: "power1.out",
+        delay:0.5
+    },"loader");
+    tl1.to("#loader", {
+        display:"none",
+        duration: 0.01
+    });
+    tl1.from("nav ul li", {
+        y:-40,
+        duration: 0.8,
+        stagger:0.1
+    });
+
+}
+loaderAnimation()
+
+
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     center:false,
+//     video:true,
+//     dots:false,
+//     autoplay:true,
+//     lazyLoad:true,
+//     responsiveClass:true,
+//     responsive:{
+//         0:{
+//             items:3,
+//             nav:true
+//         }
+//     }
+// })
