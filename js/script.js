46bb4e9a44b7e411a6bfa87b1f47172de7bfed16
Mkishore219 <152087766+Mkishore219@users.noreply.github.com>
2025-05-28 19:19:29 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,20 @@ window.addEventListener("load", function () {
 
 
 function lenisScroll() {
+
+    const lenis = new Lenis({
+        duration: 1.2,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+    });
+
+    lenis.on('scroll', ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+        lenis.raf(time * 1000);
+    });
+
+    gsap.ticker.lagSmoothing(0);
+
 }
 lenisScroll()
 
